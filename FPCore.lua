67ec36813da893272fe62704d78dbd454c0fac18
Richie <108384896+richie-scripts@users.noreply.github.com>
2022-11-29 13:16:22 +0800

    Updates

---
+++ FPCore.lua
@@ -0,0 +1,67 @@
+
+
+-- Currently only tested and working on Wrath
+local _, _, _, tocVersion = GetBuildInfo();
+if (tocVersion < 30000 and tocVersion > 40000) then
+	print("|cFF00ff00FilthyPrio|r: This addon currently only functions on the Wrath Classic client.")
+	return
+end
+
+local addonName, addon = ...
+local addon.new = LibStub("AceAddon-3.0"):NewAddon("FilthyPrio", "AceComm-3.0")
+local FP = addon.new
+
+FP.commPrefix = "FPrio"
+FP.hasAddon = {}
+FP.realm = GetRealmName();
+FP.faction = UnitFactionGroup("player")
+FP.serializer = LibStub:GetLibrary("AceSerializer-3.0")
+FP.version = GetAddOnMetadata("FilthyPrio", "Version") or 9999
+FP.latestVersion = version
+FP.loadTime = 0
+
+function FP:OnInitialize()
+	-- Initialise Addon
+    self.db = LibStub("AceDB-3.0"):New("PrioDB", FP.prioDefaults, "Default");
+
+	self:RegisterComm(self.commPrefix);
+	self.loadTime = GetServerTime();
+	self.db.profile.loadTime = GetServerTime();
+
+end
+
+
+FP.prioDefaults = {
+  profile = {
+    setting = true,
+  }
+}
+
+function FilthyPrio:OnInitialize()
+  -- Assuming the .toc says ## SavedVariables: MyAddonDB
+  self.db = LibStub("AceDB-3.0"):New("MyAddonDB", defaults, true)
+  db = self.db.profile
+end
+
+-- GLOBALS: LibStub, PrioDB
+-- Register an item in the prio DB
+local type, pairs, tonumber = type, pairs, tonumber
+local defaults = {
+	profile = {
+		modules = { ["*"] = true, ["EnemyCasts"] = false },
+		hidesamwise = true,
+		sparkcolor = {1, 1, 1, 0.5},
+		spelltextcolor = {1, 1, 1},
+		timetextcolor = {1, 1, 1},
+		castingcolor = {1.0, 0.49, 0},
+		channelingcolor = {0.32, 0.3, 1},
+		completecolor = {0.12, 0.86, 0.15},
+		failcolor = {1.0, 0.09, 0},
+		backgroundcolor = {0, 0, 0},
+		bordercolor = {0, 0, 0},
+		backgroundalpha = 1,
+		borderalpha = 1,
+		casttimeprecision = 1,
+	},
+}
+
---
+++ data/FPPhase2.lua
@@ -0,0 +1,431 @@
+
+function FP:buildPhase2Data()
+	local items = {
+Band of Lights
+Breastplate of the Timeless
+Dark Matter
+Drape of the Messenger
+Gloves of the Endless Dark
+Meteorite Crystal
+Nebula Band
+Observer's Mantle
+Pendant of the Somber Witness
+Pulsar Gloves
+Reply-Code Alpha
+Shoulderplates of the Celestial Watch
+Starfall Girdle
+Starlight Treads
+Strength of the Heavens
+Zodiac Leggings
+Boundless Gaze
+Bulwark of Algalon
+Comet's Trail
+Constellus
+Cosmos
+Legplates of the Endless Void
+Pharos Gloves
+Planewalker Treads
+Reply-Code Alpha
+Sabatons of Lifeless Night
+Solar Bindings
+Star-beaded Clutch
+Starwatcher's Binding
+Fang of Oblivion
+Dreambinder
+Starshard Edge
+Skyforge Crossbow
+Fragment of Val'anyr
+Archaedas' Lost Legplates
+Chestplate of Titanic Fury
+Cover of the Keepers
+Elemental Focus Stone
+Ironaya's Discarded Mantle
+Mantle of the Preserver
+Nimble Climber's Belt
+Nurturing Touch
+Raiments of the Corrupted
+Shieldwall of the Breaker
+Amice of the Stoic Watch
+Cloak of the Makers
+Cowl of the Absolute
+Gloves of the Stonereaper
+Greaves of the Rockmender
+Libram of the Resolute
+Platinum Band of the Aesir
+Ring of the Faithful Servant
+Sandals of the Ancient Keeper
+Shoulderplates of the Eternal
+Unbreakable Chestguard
+Unwavering Stare
+Runescribed Blade
+Siren's Cry
+Stonerender
+Belt of the Iron Servant
+Boots of the Petrified Forest
+Circlet of True Sight
+Cloak of the Iron Council
+Greaves of Iron Intensity
+Lady Maye's Sapphire Ring
+Leggings of Swift Reflexes
+Rune-Etched Nightblade
+Runetouch Wristwraps
+Stormtip
+Belt of the Crystal Tree
+Loop of the Agile
+Watchful Eye of Fate
+Perilous Bite
+The Masticator
+Ancient Iron Heaume
+Drape of the Lithe
+Handguards of the Enclave
+Insurmountable Fervor
+Iron-studded Mantle
+Overload Legwraps
+Phaelia's Vestments of the Sprouting Seed
+Radiant Seal
+Raiments of the Iron Council
+Runed Ironhide Boots
+Runeshaper's Gloves
+Steelbreaker's Embrace
+Unblinking Eye
+Rapture
+Stormrune Edge
+Belt of Colossal Rage
+Drape of Mortal Downfall
+Greaves of Swift Vengeance
+Sapphire Amulet of Renewal
+Shoulderpads of the Intruder
+Chestguard of the Lasher
+Gloves of the Wayward Conqueror
+Gloves of the Wayward Protector
+Gloves of the Wayward Vanquisher
+Ironbark Faceguard
+Legplates of Flourishing Resolve
+Tunic of the Limber Stalker
+Unraveling Reach
+Fire Orchid Signet
+Gloves of Whispering Winds
+Seed of Budding Carnage
+Petrified Ivy Sprig
+Serilas, Blood Blade of Invar One-Arm
+Boots of the Servant
+Gauntlets of Ruthless Reprisal
+Leggings of the Lifetender
+Legplates of the Wayward Conqueror
+Legplates of the Wayward Protector
+Legplates of the Wayward Vanquisher
+Nymph Heart Charm
+The Lifebinder
+Bladetwister
+Bronze Pendant of the Vanir
+Drape of the Sullen Goddess
+Handguards of Revitalization
+Leggings of the Enslaved Idol
+Avalanche
+Cowl of Icy Breaths
+Leggings of the Wayward Conqueror
+Leggings of the Wayward Protector
+Leggings of the Wayward Vanquisher
+Signet of Winter
+Stormedge
+Winter's Frigid Embrace
+Bitter Cold Armguards
+Icecore Staff
+Ice Layered Barrier
+Shiver
+The Boreal Guard
+Breastplate of the Wayward Conqueror
+Breastplate of the Wayward Protector
+Breastplate of the Wayward Vanquisher
+Frost-bound Chain Bracers
+Frostplate Greaves
+Frozen Loop
+Northern Barrier
+Winter's Icy Embrace
+Bindings of Winter Gale
+Constellus
+Drape of Icy Intent
+Frigid Strength of Hodir
+Gloves of the Frozen Glade
+Staff of Endless Winter
+Armbraces of the Vibrant Flame
+Drape of Fuming Anger
+Furnace Stone
+Gauntlets of the Iron Furnace
+Gloves of Smoldering Touch
+Igniter Rod
+Pauldrons of Tempered Will
+Relentless Edge
+Rifle of the Platinum Guard
+Shawl of the Caretaker
+Charred Saronite Greaves
+Cindershard Ring
+Flamestalker Boots
+Flamewrought Cinch
+Girdle of Embers
+Heart of Iron
+Helm of the Furnace Master
+Lifeforge Breastplate
+Pyrelight Circle
+Soot-Covered Mantle
+Totem of the Dancing Flame
+Wristguards of the Firetender
+Intensity
+Scepter of Creation
+Worldcarver
+Emerald Signet Ring
+Greaves of the Earthbinder
+Mark of the Unyielding
+Pendant of the Piercing Glare
+Sabatons of the Iron Watcher
+Shawl of the Shattered Giant
+Shoulderguards of the Solemn Watch
+Spark of Hope
+Spire of Withering Dreams
+Stoneguard
+Bracers of Unleashed Magic
+Gloves of the Pythonic Guardian
+Handwraps of Plentiful Recovery
+Idol of the Crying Wind
+Ironmender
+Leggings of the Stoneweaver
+Necklace of Unerring Mettle
+Robes of the Umbral Brute
+Saronite Plated Legguards
+Shoulderpads of the Monolith
+Unfaltering Armguards
+Wrathstone
+Giant's Bane
+Malice
+Decimator's Armguards
+Combustion Bracers
+Energy Siphon
+Firesoul
+Firestrider Chestguard
+Flamewatch Armguards
+Ironsoul
+Kinetic Ripper
+Lifespark Visage
+Might of the Leviathan
+Pyrite Infuser
+Gilded Steel Legplates
+Handguards of Potent Cures
+Mantle of Fiery Vengeance
+Shimmering Seal
+Twirling Blades
+Constructor's Handwraps
+Embrace of the Leviathan
+Freya's Choker of Warding
+Gloves of the Fiery Behemoth
+Glowing Ring of Reclamation
+Iron Riveted War Helm
+Leviathan Fueling Manual
+Mechanist's Bindings
+Mimiron's Inferno Couplings
+Steamcaller's Totem
+Steamworker's Goggles
+Strength of the Automaton
+The Leviathan's Coil
+Rising Sun
+Titanguard
+Boots of Fiery Resolution
+Golden Saronite Dragon
+Pendant of Fiery Havoc
+Plated Leggings of Ruination
+Shoulderpads of Dormant Energies
+Cable of the Metrognome
+Helm of the Wayward Conqueror
+Helm of the Wayward Vanquisher
+Pulse Baton
+Static Charge Handwraps
+Stylish Power Cape
+Helm of the Wayward Protector
+Shoulderguards of Assimilation
+Fused Alloy Legplates
+Greaves of the Iron Army
+Mimiron's Flight Goggles
+Tempered Mercury Greaves
+Fusion Blade
+Asimov's Drape
+Gauntlets of the Wayward Conqueror
+Gauntlets of the Wayward Vanquisher
+Pandora's Plea
+Waistguard of the Creator
+Gauntlets of the Wayward Protector
+Malleable Steelweave Mantle
+Insanity's Grip
+Delirium's Touch
+Armbands of Bedlam
+Conductive Seal
+Crown of Luminescence
+Titanskin Cloak
+Band of Draconic Guile
+Binding of the Dragon Matriarch
+Bracers of the Smothering Inferno
+Breastplate of the Afterlife
+Dragonsteel Faceplate
+Eye of the Broodmother
+Ironscale Leggings
+Razorscale Talon
+Stormtempered Girdle
+Treads of the Invader
+Belt of the Fallen Wyrm
+Bracers of the Broodmother
+Collar of the Wyrmhunter
+Dragonslayer's Brace
+Drape of the Drakerider
+Libram of Discord
+Living Flame
+Proto-hide Leggings
+Razorscale Shoulderguards
+Saronite Mesh Legguards
+Shackles of the Odalisque
+Sigil of Deflection
+Guiding Star
+Remorse
+Veranus' Bane
+Belt of the Blood Pit
+Guise of the Midgard Serpent
+Handwraps of Resonance
+Legacy of Thunder
+Leggings of Unstable Discharge
+Spaulders of the Wayward Conqueror
+Spaulders of the Wayward Protector
+Spaulders of the Wayward Vanquisher
+Gauntlets of the Thunder God
+Mjolnir Runestone
+Pendant of the Shallow Grave
+Sif's Remembrance
+Combatant's Bootblade
+Belt of the Betrayed
+Crown of the Wayward Conqueror
+Crown of the Wayward Protector
+Crown of the Wayward Vanquisher
+Leggings of Lost Love
+Scale of Fates
+Sif's Promise
+Vulmir, the Northern Tempest
+Embrace of the Gladiator
+Fate's Clutch
+Pauldrons of the Combatant
+Warhelm of the Champion
+Wisdom's Hold
+Bindings of the Depths
+Choker of the Abyss
+Darkstone Ring
+Gauntlets of the Wretched
+Hoperender
+Pendant of Endless Despair
+Saronite Animus Cloak
+Shadowbite
+Underworld Mantle
+Vestments of the Piercing Light
+Drape of the Faceless General
+Leggings of Profound Darkness
+Aesuga, Hand of the Ardent Champion
+Tortured Earth
+Void Sabre
+Belt of Clinging Hope
+Belt of the Darkspeaker
+Boots of the Forgotten Depths
+Boots of the Underdweller
+Darkcore Leggings
+Grips of the Unbroken
+Helm of the Faceless
+Idol of the Corruptor
+Libram of the Sacred Shield
+Mantle of the Unknowing
+Metallic Loop of the Sufferer
+Ring of the Vacant Eye
+The General's Heart
+Scepter of Lost Souls
+Lotrafen, Spear of the Damned
+Flare of the Heavens
+Handwraps of the Vigilant
+Pendulum of Infinity
+Vestments of the Blind Denizen
+Voldrethar, Dark Blade of Oblivion
+Armbands of the Construct
+Chestplate of Vicious Potency
+Conductive Cord
+Gloves of Taut Grip
+Helm of Veiled Energies
+Plasma Foil
+Power Enhancing Loop
+Pulsing Spellshield
+Treacherous Shoulderpads
+Vest of the Glowing Crescent
+Breastplate of the Stoneshaper
+Fluxing Energy Coils
+Seal of Ulduar
+Aesir's Edge
+Magnetized Projectile Emitter
+Boots of Hasty Revival
+Brass-lined Boots
+Clockwork Legplates
+Crazed Construct Ring
+Horologist's Wristguards
+Mantle of Wavering Calm
+Quartz-studded Harness
+Sandals of Rash Temperament
+Shoulderplates of the Deconstructor
+Sigil of the Vengeful Heart
+Signet of the Earthshaker
+Thunderfall Totem
+Golem-Shard Sticker
+Quartz Crystal Wand
+Twisted Visage
+Breastplate of the Devoted
+Charm of Meticulous Timing
+Gloves of the Steady Hand
+Grasps of Reason
+Sorthalis, Hammer of the Watchers
+Abaddon
+Chestguard of the Wayward Conqueror
+Chestguard of the Wayward Protector
+Chestguard of the Wayward Vanquisher
+Deliverance
+Devotion
+Faceguard of the Eyeless Horror
+Kingsbane
+Leggings of the Insatiable
+Pendant of a Thousand Maws
+Royal Seal of King Llane
+Touch of Madness
+Treads of the Dragon Council
+Amice of Inconceivable Horror
+Signet of Soft Lament
+Soul-Devouring Cinch
+Vanquished Clutches of Yogg-Saron
+Caress of Insanity
+Blood of the Old God
+Chestguard of Insidious Intent
+Chestguard of the Fallen God
+Cowl of Dark Whispers
+Garona's Guise
+Godbane Signet
+Mantle of the Wayward Conqueror
+Mantle of the Wayward Protector
+Mantle of the Wayward Vanquisher
+Sanity's Bond
+Shawl of Haunted Memories
+Soulscribe
+Earthshaper
+Hammer of Crushing Whispers
+Dark Edge of Depravity
+Legguards of Cunning Deception
+Seal of the Betrayed King
+Show of Faith
+Treads of the False Oracle
+Mimiron's Head
+	}
+
+	for k, v in pairs(defaults) do
+		if (not self.db.global[NWB.realm][NWB.faction][k]) then
+			--Add default values if no value is already set.
+			self.db.global[NWB.realm][NWB.faction][k] = v;
+		end
+	end
+	--Timer data is stored within the ace3 global table but I create a shortcut here "NWB.data".
+	self.data = self.db.global[NWB.realm][NWB.faction];
+end
